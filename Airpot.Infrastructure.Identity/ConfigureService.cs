701cf4dbb729a1a2794e8bfc498147879f96a062
Zahrakhorram142 <144644373+Zahrakhorram142@users.noreply.github.com>
2024-05-14 10:42:25 +0330

    Add Package Identity

--- Airpot.Infrastructure.Identity/ConfigureService.cs
+++ Airpot.Infrastructure.Identity/ConfigureService.cs
@@ -14,6 +14,7 @@ public static class ConfigureService
         connectionString,
                    b => b.MigrationsAssembly(typeof(ApplicationIdentityContext).Assembly.FullName)));
         return services;
+
     }
     
 }
