5553ee26624841d743fcdd33269faa5b735908ee
Zahrakhorram142 <144644373+Zahrakhorram142@users.noreply.github.com>
2024-06-01 18:32:53 +0330

    add all

--- Airport.Infrastructure/Persistance/Context/ApplicationDbContext.cs
+++ Airport.Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
+
+
     }
 
     public DbSet<Airplane> Airplanes { get; set; }
